68a130fe45bded3598e9226eb0ae7b6501249624
Horsen121 <vova.mitychin@mail.ru>
2024-12-14 13:34:17 +0700

    init

--- src/main/java/ru/nsu/array/PersistentArray.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu.array;
-
-import ru.nsu.util.PersistentCollection;
-
-/**
- * Персистентный массив
- */
-public class PersistentArray<E> implements PersistentCollection<E> {
-}
--- src/main/java/ru/nsu/map/PersistentHashMap.java
+++
@@ -1,6 +0,0 @@
-package ru.nsu.map;
-
-import ru.nsu.util.PersistentMap;
-
-public class PersistentHashMap<K, V> implements PersistentMap<K, V> {
-}
